e972523ef780f3649cf647924f860d00d1155b45
Phil Leggetter <phil@leggetter.co.uk>
2016-04-20 13:44:06 +0100

    Remove unnecessary XSockets code. Add comments.

--- ASP.NET MVC5 Realtime Chat/Scripts/chat/XSocketsChat.js
+++ ASP.NET MVC5 Realtime Chat/Scripts/chat/XSocketsChat.js
@@ -1,29 +1,38 @@
 ﻿function XSocketsChat() {
     AbstractChat.call(this);
 
+    // Connect and identify the XSockets controller to connect to: `chat`
     var conn = new XSockets.WebSocket('ws://localhost:4502', ['chat']);
+
+    // Get a reference to the controller
     this.controller = conn.controller('chat');
 
     //Setup a subscription for the topic "chatmessage"
-    this.controller.subscribe('chatmessage', this._messageReceived.bind(this));
+    this.controller.subscribe('chatmessage', this.addMessage.bind(this));
 
     this.fetchInitialMessages();
 }
 extend(XSocketsChat).with(AbstractChat);
 
 XSocketsChat.prototype.fetchInitialMessages = function () {
-    this.controller.invoke('getallmessages')
-        .then(function (messages) {
-            messages.forEach(function (message) {
-                this._messageReceived(message);
-            }.bind(this));
-        }.bind(this));
+    // RMI - returns Promise
+    var getAll = this.controller.invoke('getallmessages')
+
+    getAll.then(function (messages) {
+
+        // Loop through existing messages
+        messages.forEach(function (message) {
+
+            // Add to the UI
+            this.addMessage(message);
+
+        }, this);
+    }.bind(this));
 };
 
-XSocketsChat.prototype._messageReceived = function (data) {
-    this.addMessage(data);
-};
-
+/**
+ * Send message to the XSockets controller.
+ */
 XSocketsChat.prototype.sendMessage = function (data) {
     this.controller.publish('chatmessage', data, this.sendMessageSuccess.bind(this));
 };
