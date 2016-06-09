c2592ce76264d2572e006d8bf35bed17466684cb
Phil Leggetter <phil@leggetter.co.uk>
2016-06-10 01:38:37 +0200

    Adding regions + PubNub link to entry page

--- ASP.NET MVC5 Realtime Chat/Controllers/ChatController.cs
+++ ASP.NET MVC5 Realtime Chat/Controllers/ChatController.cs
@@ -1,9 +1,7 @@
 ﻿using ASP.NET_MVC5_Realtime_Chat.Repos;
 using PubNubMessaging.Core;
 using PusherServer;
-using System.Configuration;
 using System.Net;
-using System.Web.Configuration;
 using System.Web.Mvc;
 
 namespace ASP.NET_MVC5_Realtime_Chat.Controllers
@@ -25,18 +23,11 @@ namespace ASP.NET_MVC5_Realtime_Chat.Controllers
             return View("XSockets", "_Chat");
         }
 
+        #region Pusher
         public ActionResult Pusher()
         {
             ViewBag.PusherKey = Config.PusherAppKey;
             return View("Pusher", "_Chat");
-        }
-
-        [HttpGet, OutputCache(NoStore = true, Duration = 0, VaryByParam = "None")]
-        public ActionResult Messages()
-        {
-            var repo = new ChatRepository();
-            var messages = repo.GetAll();
-            return Json(messages, JsonRequestBehavior.AllowGet);
         }
 
         [HttpPost]
@@ -59,6 +50,49 @@ namespace ASP.NET_MVC5_Realtime_Chat.Controllers
             pusher.Trigger("chat", "chatmessage", message);
 
             return Json(message);
+        }
+        #endregion
+
+        #region PubNub
+        public ActionResult PubNub()
+        {
+            ViewBag.PubNubPublishKey = Config.PubNubPublishKey;
+            ViewBag.PubNubSubscribeKey = Config.PubNubSubscribeKey;
+            return View("PubNub", "_Chat");
+        }
+
+        [HttpPost]
+        public ActionResult PubNubMessage()
+        {
+            var text = Request.Form["text"];
+            var username = Request.Form["username"];
+
+            var repo = new ChatRepository();
+            var message = repo.CreateMessage(username, text);
+
+            // Channel, data payload - PubSub
+            // Library deals with serialising `message` object
+            Pubnub pubnub = new Pubnub(
+                Config.PubNubPublishKey,
+                Config.PubNubSubscribeKey
+            );
+            pubnub.Publish(
+                "chat",
+                message,
+                (string result) => { },
+                (PubnubClientError e) => { }
+            );
+
+            return Json(message);
+        }
+        #endregion
+
+        [HttpGet, OutputCache(NoStore = true, Duration = 0, VaryByParam = "None")]
+        public ActionResult Messages()
+        {
+            var repo = new ChatRepository();
+            var messages = repo.GetAll();
+            return Json(messages, JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult NexmoInboundSMS()
@@ -88,45 +122,6 @@ namespace ASP.NET_MVC5_Realtime_Chat.Controllers
             );
 
             return new HttpStatusCodeResult(HttpStatusCode.OK);
-        }
-
-
-        public ActionResult PubNub()
-        {
-            ViewBag.PubNubPublishKey = Config.PubNubPublishKey;
-            ViewBag.PubNubSubscribeKey = Config.PubNubSubscribeKey;
-            return View("PubNub", "_Chat");
-        }
-
-        [HttpPost]
-        public ActionResult PubNubMessage()
-        {
-            var repo = new ChatRepository();
-            var pusher = new Pusher(
-                Config.PusherAppId,
-                Config.PusherAppKey,
-                Config.PusherAppSecret
-                );
-
-            var text = Request.Form["text"];
-            var username = Request.Form["username"];
-
-            var message = repo.CreateMessage(username, text);
-
-            // Channel, event, data payload - evented PubSub
-            // Library deals with serialising `message` object
-            Pubnub pubnub = new Pubnub(
-                Config.PubNubPublishKey,
-                Config.PubNubSubscribeKey
-            );
-            pubnub.Publish(
-                "chat",
-                message,
-                (string result) => { },
-                (PubnubClientError e) => { }
-            );
-
-            return Json(message);
         }
     }
 }
